2bafc3526d8024e43113e35233b96aea756f6f76
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-09-01 10:36:19 +0000

    LandingSite development begun

--- wisebeat-frontend/src/App.js
+++ wisebeat-frontend/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import LandingSite from './components/LandingSite/LandingSite';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="app"> Hello World </h1>
+      <Switch>
+        <Route exact path="/" component={ LandingSite } />
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ wisebeat-frontend/src/components/LandingSite/LandingSite.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const LandingSite = () => {
+    return (
+        <div>
+            <h1 className="landing-site">LandingSite</h1>
+        </div>
+    )
+};
+
+export default LandingSite;
